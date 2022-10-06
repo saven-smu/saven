c1c6d89cd69a0acb704ddd250e7ed8296589a06d
Jenna Cheo <jennacheo@gmail.com>
2022-10-06 22:38:09 +0800

    feat(leaderboard.vue): add leaderboard component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,16 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const incrementCount = () => {
+  count.value++
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="incrementCount">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
---
+++ src/components/Leaderboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto">
+        leaderboard
+    </div>
+    <!-- <p>{{ arrayOfItems[0] }}</p>
+    <p>{{ arrayOfItems[1] }}</p>
+    <p>{{ arrayOfItems[2] }}</p>
+    <p>{{ arrayOfItems[3] }}</p>
+    <p>{{ arrayOfItems[4] }}</p> -->
+    <p v-for="(item, idx) in arrayOfItems" :key="idx">{{ item }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+    const arrayOfItems = ref(["string", "string2", "string3", "string4", "gay"])
+</script>
+
+<style scoped>
+
+</style>
