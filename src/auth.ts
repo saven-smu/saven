e21501a798cecc0836cd4cca6f9fb46d1f4ec822
Syoongy <mastersyong@gmail.com>
2022-10-11 00:47:25 +0800

    feat(auth0): add Auth0 login/logout support

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import { createAuth0 } from "@auth0/auth0-vue";
 
 const auth = createAuth0({
-    // audience: import.meta.env.VITE_AUTH_AUDIENCE,
+    audience: import.meta.env.VITE_AUTH_AUDIENCE,
     domain: import.meta.env.VITE_AUTH_DOMAIN,
     client_id: import.meta.env.VITE_AUTH_CLIENT_ID,
     redirect_uri: window.location.origin,
