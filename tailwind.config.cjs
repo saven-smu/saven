c1044a3497b41292498fb4b1b7f64aba485936a3
Syoongy <mastersyong@gmail.com>
2022-10-10 14:34:11 +0800

    feat(tailwind.config.cjs): add colour theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,30 @@ module.exports = {
     theme: {
         extend: {},
     },
+    daisyui: {
+        themes: [
+            {
+                saven: {
+                    primary: "#A0CED9",
+
+                    secondary: "#FFC09F",
+
+                    accent: "#ADF7B6",
+
+                    neutral: "#FCF5C7",
+
+                    "base-100": "#FFF",
+
+                    info: "#3ABFF8",
+
+                    success: "#36D399",
+
+                    warning: "#FBBD23",
+
+                    error: "#F87272",
+                },
+            },
+        ],
+    },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
