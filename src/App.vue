3cc08b0fdc5084af5ba63ed8c933f92242d947d2
Syoongy <mastersyong@gmail.com>
2022-10-10 15:03:34 +0800

    feat(saven design): update styling to match figma

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import Leaderboard from "./components/Leaderboard.vue";
 </script>
 
 <template>
     <main>
-        <div class="mt-4 w-full px-2"><router-view /></div>
+        <div class="w-full"><router-view /></div>
     </main>
 </template>
 
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,23 +1,19 @@
 <template>
     <SignUpNavbar />
-    <!-- <HelloWorld msg="Now with vue-router!" /> -->
-    <!-- <router-link class="btn btn-primary" to="/leaderboard">To Leaderboard!</router-link> -->
-    <div class="hero min-h-screen bg-gradient-to-b from-[#A0CED9] to-white">
+    <div class="hero bg-gradient-to-b from-primary to-white">
         <div class="hero-content flex-col lg:flex-row-reverse">
             <img src="../assets/saven_homepage.png" width="700" height="700" />
             <div>
                 <!-- how to change text colour help -->
-                <h1 class="text-5xl font-bold">Welcome to SavEn</h1>
+                <h1 class="text-5xl font-bold text-secondary">Welcome to SavEn</h1>
                 <p class="py-6">
                     Provident cupiditate voluptatem et in. Quaerat fugiat ut
                     assumenda excepturi exercitationem quasi. In deleniti eaque
                     aut repudiandae et a id nisi.
                 </p>
-                <router-link to="/LogInPage"
-                    ><button class="btn btn-primary">
-                        Get Started
-                    </button></router-link
-                >
+                <router-link to="/LogInPage">
+                    <button class="btn btn-secondary">Get Started</button>
+                </router-link>
             </div>
         </div>
     </div>
