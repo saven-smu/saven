1a9dedc90cae44a41faa9b68b508d544a9aa85e3
Syoongy <mastersyong@gmail.com>
2022-10-11 01:07:31 +0800

    refactor(leaderboardpage.vue): remove unused router-link button

--- src/views/LeaderboardPage.vue
+++ src/views/LeaderboardPage.vue
@@ -1,12 +1,9 @@
 <template>
     <Leaderboard />
-    <router-link class="btn btn-primary" to="/">To Landing!</router-link>
 </template>
 
 <script setup lang="ts">
-    import Leaderboard from "../components/Leaderboard.vue";
+import Leaderboard from "../components/Leaderboard.vue";
 </script>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
