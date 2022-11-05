ddd7cd4235e863b635adef1fc8dadc067efb8092
Syoongy <mastersyong@gmail.com>
2022-11-05 18:22:03 +0800

    feat(analyticspage.vue): retrieve user bills and store in a Map

--- src/views/AnalyticsPage.vue
+++ src/views/AnalyticsPage.vue
@@ -1,13 +1,41 @@
 <template>
     <div class="bg-gradient-to-b from-primary to-white">
-        <div class="container mx-auto pt-8">{{ auth }}</div>
+        <div class="container mx-auto pt-8">{{ user }}</div>
+        <button class="btn-primary btn" @click="getBills">getBills</button>
     </div>
 </template>
 
 <script setup lang="ts">
-import auth from "../auth";
+import { useUserStore } from "../stores/user";
+import { storeToRefs } from "pinia";
+import { getBillsByUserIDAndDays } from "../composables/api/bill";
+import { Bill } from "../types/bill";
+import { ref } from "vue";
 
-const { user } = auth;
+const { user } = storeToRefs(useUserStore());
+const bills = ref<Map<string, Bill[]>>(new Map());
+
+const getBills = async () => {
+    if (user.value) {
+        const res = await getBillsByUserIDAndDays(user.value.id, 7);
+        if (res) {
+            const formatter = new Intl.DateTimeFormat("en-SG");
+            for (const bill of res) {
+                const billDate = new Date(bill.storedDateTime);
+                const date = formatter.format(billDate);
+                let dataToPush = bills.value.get(date);
+                if (dataToPush) {
+                    dataToPush.push(bill);
+                } else {
+                    dataToPush = [bill];
+                }
+
+                bills.value.set(date, dataToPush);
+            }
+            console.log(bills.value);
+        }
+    }
+};
 </script>
 
 <style scoped></style>
