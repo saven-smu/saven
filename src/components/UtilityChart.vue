5600b74571c2e830aab46aea1200f73cef6e1baf
Syoongy <mastersyong@gmail.com>
2022-11-06 13:08:28 +0800

    feat(utilitychart.vue): add generic UtilityChart component

---
+++ src/components/UtilityChart.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Line />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Bill } from '../types/bill';
+import { Line } from "vue-chartjs"
+import { computed } from '@vue/reactivity';
+
+defineProps<{ utilityDataMap: Map<string, Bill[]> }>();
+
+const chartData = computed(() => {
+    const retData = [];
+})
+
+</script>
+
+<style scoped>
+
+</style>
