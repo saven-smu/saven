32129823c6b66e209f829be3d34bcc0fcad2442f
Syoongy <mastersyong@gmail.com>
2022-11-07 15:09:00 +0800

    feat(auth.ts): use localStorage as auth0 cache

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,7 @@ const auth = createAuth0({
     domain: import.meta.env.VITE_AUTH_DOMAIN,
     client_id: import.meta.env.VITE_AUTH_CLIENT_ID,
     redirect_uri: window.location.origin,
+    cacheLocation: "localstorage",
 });
 
 export default auth;
