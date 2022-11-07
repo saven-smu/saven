939fefe18538adafea45aed537e915692e3b3125
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2022-11-08 02:05:21 +0800

    refactor(leaderboardpage.vue): remove tabs

--- src/views/LeaderboardPage.vue
+++ src/views/LeaderboardPage.vue
@@ -1,12 +1,4 @@
 <template>
-    <div class="tabs">
-        <div class="absolute inset-x-0 top-10 h-16 ...">
-            <a class="tab tab-bordered">Tab 1</a>
-            <a class="tab tab-bordered tab-active">Tab 2</a>
-            <a class="tab tab-bordered">Tab 3</a>
-        </div>
-    </div>
-
     <div class="container mx-auto">
         <Leaderboard v-motion-slide-top />
     </div>
