1da87738cb5af8be1056599daeeec22a29316efd
Syoongy <mastersyong@gmail.com>
2022-11-08 16:05:30 +0800

    feat: reroute logout

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ const auth = createAuth0({
     audience: import.meta.env.VITE_AUTH_AUDIENCE,
     domain: import.meta.env.VITE_AUTH_DOMAIN,
     client_id: import.meta.env.VITE_AUTH_CLIENT_ID,
-    redirect_uri: window.location.origin,
+    redirect_uri: "https://saven.pages.dev",
     cacheLocation: "localstorage",
 });
 
