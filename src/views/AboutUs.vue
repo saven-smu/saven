c2045f06a3786b0351fec45909bb9c799788084b
jechaxu <95958316+jechaxu@users.noreply.github.com>
2022-11-08 14:37:42 +0800

    fix(aboutus.vue): fixed bulby pics

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -19,10 +19,10 @@
             </div>
         </div>
 
-        <div class="text-center pt-20" ref="textRef">
+        <div class="text-center pt-20 mx-auto" ref="textRef">
             <h1 class="text-5xl font-bold">WHERE IT BEGAN</h1>
-            <div class="flex flex-row">
-                <p class="text-left pt-10 pb-20 mx-auto mb-5 max-w-lg">With climate change becoming an increasingly
+            <div class="flex items-center flex-row">
+                <p class="text-left pt-10 pb-10 mb-5 max-w-lg">With climate change becoming an increasingly
                     prevalent
                     issue, our small, tight knit team turned our attention towards efforts to conserve the
                     environmental. Thus, Saven was started because of our wish to address the increasing problem of
@@ -35,7 +35,7 @@
                     We want to continue advocating and raising awareness about the importance of energy
                     conservation,
                     and helping you to manage your utility bills is only our first step.</p>
-                <div class="">
+                <div class="pb-10 items-center">
                     <img src="../assets/saven_family.png" width="400" />
                 </div>
             </div>
@@ -43,11 +43,11 @@
 
         <div class="" ref="teamRef">
             <h1 class="text-center text-5xl font-bold pt-10">MEET THE TEAM</h1>
-            <div class="pb-20">
+            <div class="pb-20" ref="teamRef" >
                 <div class="py-15 px-15 flex flex-row justify-center items-center">
                     <div class="py-10 px-10">
                         <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                            <figure><img src="../assets/syong.jpg" width="300" ref="imgRef" /></figure>
+                            <figure><img src="../assets/syong.jpg" width="300" /></figure>
                             <div class="card-body">
                                 <h2 class="text-2xl text-center"><b>Fan Syong Yue</b></h2>
                                 <p class="text-center"><i>Frontend Carry</i></p>
@@ -55,7 +55,7 @@
                         </div>
                     </div>
                     <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                        <figure><img src="../assets/jeff.jpg" width="300" ref="imgRef" /></figure>
+                        <figure><img src="../assets/jeff.jpg" width="300"/></figure>
                         <div class="card-body">
                             <h2 class="text-2xl text-center"><b>Jeffery Lau</b></h2>
                             <p class="text-center"><i>Backend Carry</i></p>
@@ -63,10 +63,10 @@
                     </div>
                 </div>
 
-                <div class="py-15 flex flex-row justify-center items-center">
+                <div class="py-15 flex flex-row justify-center items-center" >
                     <div class="px-10 ">
                         <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                            <figure><img src="../assets/josh.jpg" width="300" ref="imgRef" /></figure>
+                            <figure><img src="../assets/josh.jpg" width="300"/></figure>
                             <div class="card-body">
                                 <h2 class="text-2xl text-center"><b>Joshua Chia</b></h2>
                                 <p class="text-center"><i>Doing his best</i></p>
@@ -76,7 +76,7 @@
 
                     <div class="px-10">
                         <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                            <figure><img src="../assets/kai.jpg" width="300" ref="imgRef" /></figure>
+                            <figure><img src="../assets/kai.jpg" width="300"/></figure>
                             <div class="card-body">
                                 <h2 class="text-2xl text-center"><b>Hei Yee Kai</b></h2>
                                 <p class="text-center"><i>same bro</i></p>
@@ -86,7 +86,7 @@
 
                     <div class="px-10">
                         <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                            <figure><img src="../assets/jenna.jpg" width="300" ref="imgRef" /></figure>
+                            <figure><img src="../assets/jenna.jpg" width="300"/></figure>
                             <div class="card-body">
                                 <h2 class="text-2xl text-center"><b>Jenna Cheo</b></h2>
                                 <p class="text-center"><i>Never touching uiux again</i></p>
@@ -104,6 +104,7 @@ import { useMotion } from "@vueuse/motion";
 import { ref } from "vue";
 
 const carouselRef = ref();
+// const imgRef = ref();
 const textRef = ref();
 const teamRef = ref();
 
@@ -140,6 +141,35 @@ useMotion(textRef, {
         },
     },
 });
+
+// const imgRet = useMotion(imgRef, {
+//     initial: {
+//         y: 100,
+//         opacity: 0,
+//     },
+//     enter: {
+//         y: 0,
+//         opacity: 1,
+//         transition: {
+//             type: "spring",
+//             stiffness: 350,
+//             damping: 20,
+//             delay: 500,
+//             onComplete: () => {
+//                 imgRet.variant.value = "levitate";
+//             },
+//         },
+//     },
+//     levitate: {
+//         y: 10,
+//         transition: {
+//             duration: 2000,
+//             repeat: Infinity,
+//             ease: "easeInOut",
+//             repeatType: "mirror",
+//         },
+//     },
+// });
 
 useMotion(teamRef, {
     initial: {
