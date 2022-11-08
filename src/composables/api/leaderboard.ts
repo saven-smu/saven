68967629e9b6d2194341ab459b0db30e9a709781
jechaxu <95958316+jechaxu@users.noreply.github.com>
2022-11-08 14:21:15 +0800

    feat(aboutus.vue): added animations

--- src/composables/api/leaderboard.ts
+++ src/composables/api/leaderboard.ts
@@ -2,56 +2,18 @@ import ky, { HTTPError } from "ky";
 import auth from "../../auth";
 import { Leaderboard } from "../../types/leaderboard";
 
-// const apiURL = import.meta.env.VITE_API_URL;
+const apiURL = import.meta.env.VITE_API_URL;
 
-// const getLeaderboard = async () => {
-//     try {
-//         const res = await ky(`${apiURL}/api/userLeaderboards`, {
-//             headers: { Authorization: `Bearer ${authToken}` },
-//         }).json();
-//       return res as any;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-
-const getLeaderboard = async (userId: string, days: number) => {
+const getLeaderboard = async () => {
     try {
         const authToken = await auth.getAccessTokenSilently();
-        const res = await ky
-            .get(
-                "http://saven-lb-1696971161.ap-southeast-1.elb.amazonaws.com/api/userLeaderboards/", {
-                    headers: { Authorization: `Bearer ${authToken}` }
-                }
-            )
-            .json() as Leaderboard[];
-
-            for (const leaderboard of res) {
-                var currDay = new Date();
-                var today = String(currDay.getDate())
-                if (!(leaderboard.storedDateTime === today)) {
-                    leaderboard.display = 'none';
-                }
-            }
-
-            // const formatter = new Intl.DateTimeFormat("en-SG");
-            // const date = formatter.format(new Date(leaderboard.storedDateTime));
-
-        // return res as Leaderboard[];
+        const res = await ky(`${apiURL}/api/leaderboards`, {
+            headers: { Authorization: `Bearer ${authToken}` },
+        }).json();
+      return res as Leaderboard[];
     } catch (error) {
         console.log(error);
     }
-
 };
-
-function getStartOfWeek(date) {
-    const dt = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    const offset = dt.getDate() - (dt.getDay() === 0 ? 6: dt.getDay() - 1);
-    return new Date(dt.setDate(offset));
-}
-
-function getStartOfMonth(date) {
-    return new Date(date.getFullYear(), date.getMonth(), 1)
-}
 
 export { getLeaderboard };
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-gradient-to-b from-primary to-[#FFFFF9] flex flex-col">
-        <div class="drop-shadow-lg pt-5 px-20">
+        <div class="drop-shadow-lg pt-5 px-20" ref="carouselRef">
             <div class="carousel w-full">
                 <div id="item1" class="carousel-item w-full">
                     <img src="../assets/gang.jpg" width="w-full" />
@@ -19,7 +19,7 @@
             </div>
         </div>
 
-        <div class="text-center pt-20">
+        <div class="text-center pt-20" ref="textRef">
             <h1 class="text-5xl font-bold">WHERE IT BEGAN</h1>
             <div class="flex flex-row">
                 <p class="text-left pt-10 pb-20 mx-auto mb-5 max-w-lg">With climate change becoming an increasingly
@@ -41,55 +41,56 @@
             </div>
         </div>
 
-
-        <h1 class="text-center text-5xl font-bold pt-10">MEET THE TEAM</h1>
-        <div class="pb-20">
-            <div class="py-15 px-15 flex flex-row justify-center items-center">
-                <div class="py-10 px-10">
-                    <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                        <figure><img src="../assets/syong.jpg" width="300" ref="imgRef" /></figure>
-                        <div class="card-body">
-                            <h2 class="text-2xl text-center"><b>Fan Syong Yue</b></h2>
-                            <p class="text-center"><i>Frontend Carry</i></p>
+        <div class="" ref="teamRef">
+            <h1 class="text-center text-5xl font-bold pt-10">MEET THE TEAM</h1>
+            <div class="pb-20">
+                <div class="py-15 px-15 flex flex-row justify-center items-center">
+                    <div class="py-10 px-10">
+                        <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
+                            <figure><img src="../assets/syong.jpg" width="300" ref="imgRef" /></figure>
+                            <div class="card-body">
+                                <h2 class="text-2xl text-center"><b>Fan Syong Yue</b></h2>
+                                <p class="text-center"><i>Frontend Carry</i></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                    <figure><img src="../assets/jeff.jpg" width="300" ref="imgRef" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-2xl text-center"><b>Jeffery Lau</b></h2>
-                        <p class="text-center"><i>Backend Carry</i></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="py-15 flex flex-row justify-center items-center">
-                <div class="px-10 ">
                     <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                        <figure><img src="../assets/josh.jpg" width="300" ref="imgRef" /></figure>
+                        <figure><img src="../assets/jeff.jpg" width="300" ref="imgRef" /></figure>
                         <div class="card-body">
-                            <h2 class="text-2xl text-center"><b>Joshua Chia</b></h2>
-                            <p class="text-center"><i>Doing his best</i></p>
+                            <h2 class="text-2xl text-center"><b>Jeffery Lau</b></h2>
+                            <p class="text-center"><i>Backend Carry</i></p>
                         </div>
                     </div>
                 </div>
 
-                <div class="px-10">
-                    <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                        <figure><img src="../assets/kai.jpg" width="300" ref="imgRef" /></figure>
-                        <div class="card-body">
-                            <h2 class="text-2xl text-center"><b>Hei Yee Kai</b></h2>
-                            <p class="text-center"><i>same bro</i></p>
+                <div class="py-15 flex flex-row justify-center items-center">
+                    <div class="px-10 ">
+                        <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
+                            <figure><img src="../assets/josh.jpg" width="300" ref="imgRef" /></figure>
+                            <div class="card-body">
+                                <h2 class="text-2xl text-center"><b>Joshua Chia</b></h2>
+                                <p class="text-center"><i>Doing his best</i></p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="px-10">
-                    <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
-                        <figure><img src="../assets/jenna.jpg" width="300" ref="imgRef" /></figure>
-                        <div class="card-body">
-                            <h2 class="text-2xl text-center"><b>Jenna Cheo</b></h2>
-                            <p class="text-center"><i>Never touching uiux again</i></p>
+                    <div class="px-10">
+                        <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
+                            <figure><img src="../assets/kai.jpg" width="300" ref="imgRef" /></figure>
+                            <div class="card-body">
+                                <h2 class="text-2xl text-center"><b>Hei Yee Kai</b></h2>
+                                <p class="text-center"><i>same bro</i></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="px-10">
+                        <div class="card w-96 bg-white py-5 px-5 shadow-xl rounded">
+                            <figure><img src="../assets/jenna.jpg" width="300" ref="imgRef" /></figure>
+                            <div class="card-body">
+                                <h2 class="text-2xl text-center"><b>Jenna Cheo</b></h2>
+                                <p class="text-center"><i>Never touching uiux again</i></p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -99,7 +100,63 @@
 </template>
 
 <script setup lang="ts">
+import { useMotion } from "@vueuse/motion";
+import { ref } from "vue";
 
+const carouselRef = ref();
+const textRef = ref();
+const teamRef = ref();
+
+useMotion(carouselRef, {
+    initial: {
+        y: 100,
+        opacity: 0,
+    },
+    enter: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            stiffness: 350,
+            damping: 40,
+            delay: 300,
+        },
+    },
+});
+
+useMotion(textRef, {
+    initial: {
+        y: 100,
+        opacity: 0,
+    },
+    enter: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            stiffness: 350,
+            damping: 40,
+            delay: 500,
+        },
+    },
+});
+
+useMotion(teamRef, {
+    initial: {
+        y: 100,
+        opacity: 0,
+    },
+    enter: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            stiffness: 350,
+            damping: 40,
+            delay: 700,
+        },
+    },
+});
 </script>
 
 <style scoped>
